b59f9e63527cc7510e46e81ce6a88c91fc891d01
W-Rabbit <purplevoxop@gmail.com>
2023-02-02 16:04:37 +0200

    Add button text change prototype

---
+++ script.js
@@ -0,0 +1,9 @@
+const button1 = document.querySelector('.button1');
+button1.addEventListener('click', function(){
+    if(button1.innerText === 'spune pepsi'){
+        button1.innerText = "ma-ta-i sexi";
+    }
+    else {button1.innerText = 'spune pepsi';
+}
+}
+)
